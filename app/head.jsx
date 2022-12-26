05c16f26a051a17799558efc6f82d28a9d4a4cca
Rajnish Kumar Singh <rajnish2409gkp@gmail.com>
2022-12-26 10:00:11 +0000

    Pending changes exported from your codespace

---
+++ app/head.jsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>Hexime</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="An anime site by Hexaorzo" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
